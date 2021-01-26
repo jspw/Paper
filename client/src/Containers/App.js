eedc36ac77aa301db72831de873e21fe2c98c56d
jspw <mhshifat757@gmail.com>
2021-01-26 23:38:13 +0600

    frontend:login validation added

--- client/src/Containers/App.js
+++ client/src/Containers/App.js
@@ -1,15 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "../Components/Navbar/Navbar";
 import  MCQ  from "../Components/Exam/MCQs/MCQ/MCQ";
-// import SignIn from "../Components/Authentication/SignIn";
+import SignIn from "../Components/Authentication/SignIn";
 import SignUp from "../Components/Authentication/SignUp";
 // import  MCQ  from "../Components/Exam/MCQs/MCQ/MCQ";
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <SignIn /> */}
-      <SignUp />
+      <SignIn />
+      {/* <SignUp /> */}
       {/* <MCQ /> */}
     </div>
   );
