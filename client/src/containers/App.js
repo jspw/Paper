c618a6a0a2c77f9718cecd7ce7e6c75264cd1063
jspw <mhshifat757@gmail.com>
2021-01-26 21:40:18 +0600

    database : exported mongodb models

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "../Components/Navbar/Navbar";
-import SignIn from "../Components/Sign/SignIn";
-import SignUp from "../Components/Sign/SignUp";
+import SignIn from "../Components/Auth/SignIn";
+import SignUp from "../Components/Auth/SignUp";
 import  MCQ  from "../Components/Exam/MCQs/MCQ/MCQ";
 function App() {
   return (
